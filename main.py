5691833912be4a608d79854285df71a094da4461
Muskan <imh10017.18@bitmesra.ac.in>
2021-04-21 00:30:24 +0530

    Program with announcement audio generated

--- main.py
+++ main.py
@@ -4,14 +4,31 @@ from pydub import AudioSegment
 from gtts import gTTS
 
 def textToSpeech(text,filename):
-    '''This function '''
-    pass
+    '''This function changes the text to mp3'''
+    
+    mytext = str(text)
+    language = 'hi'
+    myobj = gTTS(text = mytext , lang=language, slow = True)
+    myobj.save(filename)
+    
+    
 
 def mergeAudios(audios):
     '''This function returns pydubs audio segment'''
-    pass
+    
+    
+    combined = AudioSegment.empty()
+    for audio in audios:
+        combined += AudioSegment.from_mp3(audio)
+    return combined
+        
+        
+        
 
 def generateSkeleton():
+    '''Slicing of audio file for different parts'''
+    
+    
     # 1st part
     audio = AudioSegment.from_mp3('railway.mp3')
     start = 88000
@@ -49,15 +66,35 @@ def generateSkeleton():
     audioProcessed = audio[start:finish]
     audioProcessed.export("11_hindi.mp3", format="mp3")
     
+    
+    
+    
 
 def generateAnnouncement(filename):
-    pass
+    '''Reads the excel file and compiles all the information to form its mp3 using textToSpeech function'''
+    
+    
+    df = pd.read_excel(filename)
+    print(df)
+    for index, item in df.iterrows():
+        textToSpeech(item['from'], '2_hindi.mp3')
+        textToSpeech(item['via'], '4_hindi.mp3')
+        textToSpeech(item['to'], '6_hindi.mp3')
+        textToSpeech(item['train_no'] + " " + item['train_name'], '8_hindi.mp3')
+        textToSpeech(item['platform'], '10_hindi.mp3')
+        
+        audios = [f"{i}_hindi.mp3" for i in range(1,12)]
+        announcement = mergeAudios(audios)
+        announcement.export(f"announcement_{item['train_no']}_{index+1}.mp3", format="mp3")
+
+        
 
 if __name__ == '__main__':
     print("Generating Skeleton...")
     generateSkeleton()
     print("Now generating Announcement")
     generateAnnouncement("announce_hindi.xlsx")
+    
 
 
 
